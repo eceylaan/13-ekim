1590ace28e1cb39acd8775463c1fba916ccaa531
eceylaan <ceylann.ece@gmail.com>
2024-10-16 18:49:00 +0300

    anasayfa css eklendi

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -16,7 +16,36 @@ export default function LoginPage() {
         </nav>
       </div>
       <div className="divider"></div>
+
+      <div className="loginFlexBox">
+        <div className="loginFlexBoxPadding">
+          <div className="loginTitle">
+            <h1>Human</h1>
+            <h1>stories & ideas</h1>
+          </div>
+          <div className="loginMiniTitle">A place to read, write, and deepen your understanding</div>
+          <div>
+            <button className="loginButton">Start Reading</button>
+          </div>
+        </div>
+        <div>
+          <img width={450} src="/assets/loginimage.webp" alt="" />
+        </div>
+      </div>
+      <div className="divider"></div>
       <div>
+        <div className="loginPageFooter">
+          <p>Help</p>
+          <p>Status</p>
+          <p>About</p>
+          <p>Careers</p>
+          <p>Press</p>
+          <p>Blog</p>
+          <p>Privacy</p>
+          <p>Term</p>
+          <p>Text to speech</p>
+          <p>Teams</p>
+        </div>
         <form>
           <label htmlFor="email">Email:</label>
           <input id="email" name="email" type="email" required />
